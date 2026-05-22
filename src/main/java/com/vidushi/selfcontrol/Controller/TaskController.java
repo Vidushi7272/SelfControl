97d5eab315f5027d6104caf4dde088f9929533cc
Vidushi Mishra <vidushimishra725@gmail.com>
2026-05-22 19:52:22 +0530

    Implemented GET API for fetching tasks

--- src/main/java/com/vidushi/selfcontrol/Controller/TaskController.java
+++ src/main/java/com/vidushi/selfcontrol/Controller/TaskController.java
@@ -1,9 +1,12 @@
 package com.vidushi.selfcontrol.Controller;
 import com.vidushi.selfcontrol.Service.TaskService;
 import com.vidushi.selfcontrol.Task;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TaskController {
@@ -14,5 +17,9 @@ public class TaskController {
     @PostMapping("/tasks")
     public Task createTask(@RequestBody Task task){
         return service.createTask(task);
+    }
+    @GetMapping("/tasks")
+    public List<Task> getTask(){
+      return service.getTask();
     }
 }
--- src/main/java/com/vidushi/selfcontrol/Service/TaskService.java
+++ src/main/java/com/vidushi/selfcontrol/Service/TaskService.java
@@ -4,12 +4,13 @@ import com.vidushi.selfcontrol.repository.TaskRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class TaskService {
     private final TaskRepository t;
     public TaskService(TaskRepository x){
-      t=x;
+        t=x;
     }
    public Task createTask(Task task){
         task.setCreatedAt(LocalDateTime.now());
@@ -17,5 +18,8 @@ public class TaskService {
             task.setDueDate(LocalDateTime.now().plusHours(24));
         }
         return t.save(task);
+   }
+   public List<Task> getTask(){
+        return t.findAll();
    }
 }
